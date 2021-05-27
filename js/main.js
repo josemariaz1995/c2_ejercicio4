b4270f74e077cb2ee36e77a333a4121731892ee4
Jose Maria Zamora Perez <jzamora@biada.net>
2021-05-27 22:02:53 +0200

    formateo eslint

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
-let palabras = ["café", "croissant", "tostada", "zumo"];
+const palabras = ["café", "croissant", "tostada", "zumo"];
 const arrayParaRellenar = [];
 
 function modificaPares(array, funcion) {
-  for (let i in array) {
+  for (const i in array) {
     if (i % 2 !== 0) {
-      console.log("2:" + array[i]);
+      console.log(`2:${array[i]}`);
       arrayParaRellenar.push(funcion(array[i]));
     } else {
       arrayParaRellenar.push(array[i]);
-      console.log("1:" + array[i]);
+      console.log(`1:${array[i]}`);
     }
   }
   return arrayParaRellenar;
 }
 
-let palabrasParesModificadas = modificaPares(
+const palabrasParesModificadas = modificaPares(
   palabras,
   (elemento) => elemento.charAt(0).toUpperCase() + elemento.slice(1)
 );
